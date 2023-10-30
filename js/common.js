9c1f71a5f16ec055b3f05c50bf1111114012f905
Sanhati <sanhatikundu02@gmail.com>
2023-10-31 01:24:50 +0530

    modified the js script

--- js/common.js
+++ js/common.js
@@ -1,46 +1,17 @@
-// // Function to show a preview of the uploaded image
-// function showImagePreview(inputElement, imagePreviewElement) {
-//     const file = inputElement.files[0];
-//     if (file) {
-//         const reader = new FileReader();
-//         reader.onload = function (event) {
-//             imagePreviewElement.src = event.target.result;
-//             imagePreviewElement.style.display = 'block';
-//         };
-//         reader.readAsDataURL(file);
-//     } else {
-//         imagePreviewElement.style.display = 'none';
-//     }
-// }
-
-// // Attach an event listener to the input element for showing the image preview
-// function attachImageInputListener(inputElement, imagePreviewElement) {
-//     inputElement.addEventListener('change', function () {
-//         showImagePreview(inputElement, imagePreviewElement);
-//     });
-// }
-
 document.addEventListener('DOMContentLoaded', function () {
     const imageInput = document.getElementById('imageInput');
     const imageInput2 = document.getElementById('imageInput2');
     const uploadButton = document.getElementById('uploadButton');
     const uploadButton2 = document.getElementById('uploadButton2');
     const originalImagePreview = document.getElementById('originalImagePreview');
-    const showOptions = document.getElementById('showOptions');
-    const encryptedImage = document.getElementById('encryptedImage');
-    const downloadEncryptedLink = document.getElementById('downloadEncryptedLink');                               
+    const showOptions = document.getElementById('showOptions');                              
     const decryptedImage = document.getElementById('decryptedImage'); 
     const decryptShow = document.getElementById('decryptShow');                                             
     const downloadDecryptedLink = document.getElementById('downloadDecryptedLink');
     const encryptButton = document.getElementById('encryptButton');
     const decryptButton = document.getElementById('decryptButton');
     const decryptionKeyInput = document.getElementById('decryptionKey');
-    const signs = document.querySelectorAll('x-sign')
-    const randomIn = (min, max) => (
-    Math.floor(Math.random() * (max - min + 1) + min)
-    )
-        
-    // let encryptionKey = null;                                                                                     
+                                                                                           
 
     // Function to show a preview of the uploaded image
     function showOriginalImagePreview(file) {
@@ -54,15 +25,13 @@ document.addEventListener('DOMContentLoaded', function () {
     reader.readAsDataURL(file);
     }
 
-    if(uploadButton){                                                                                            // Change made by Sayan - 4 (If statement added)
+    if(uploadButton){                                                                                            
         uploadButton.addEventListener('click', function () {
             // Trigger the hidden file input element to allow the user to select an image for encryption
             imageInput.click();
         });
     }
-
-
-    if(imageInput){                                                                                              // Change made by Sayan - 5 (If statement added)
+    if(imageInput){                                                                                             
         imageInput.addEventListener('change', function () {
             if (imageInput.files.length > 0) {
                 showOriginalImagePreview(imageInput.files[0]);
@@ -85,25 +54,6 @@ document.addEventListener('DOMContentLoaded', function () {
     function decryptImage(encryptedData, key) {
         return encryptImage(encryptedData, key); // XOR is its own inverse
     }
-
-    // Function to show a preview of the uploaded image
-    // function showOriginalImagePreview(file) {
-    //     const reader = new FileReader();
-    //     reader.onload = function (event) {
-    //         originalImagePreview.src = event.target.result;
-    //         originalImagePreview.style.display = 'block';
-    //     };
-    //     reader.readAsDataURL(file);
-    // }
-
-    // imageInput.addEventListener('change', function () {
-    //     if (imageInput.files.length > 0) {
-    //         showOriginalImagePreview(imageInput.files[0]);
-    //     } else {
-    //         originalImagePreview.style.display = 'none';
-    //     }
-    // });
-
     if(encryptButton){                                                                                       
         encryptButton.addEventListener('click', function () {
             if (imageInput.files.length === 0) {
@@ -126,19 +76,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 const downloadEncryptedLink = document.getElementById('downloadEncryptedLink');             
                 const encryptedData = encryptImage(imageData, encryptionKey);
                 const encryptedBlob = new Blob([encryptedData], { type: file.type });
-                // const encryptedBlob = new Blob([new Uint8Array(encryptedData.buffer)], { type: file.type });
                 downloadEncryptedLink.href = URL.createObjectURL(encryptedBlob);
                 downloadEncryptedLink.style.display = 'block';
-                // const encryptedData = encryptImage(imageData, encryptionKey);
-                // encryptedImage.src = URL.createObjectURL(new Blob([encryptedData], { type: file.type }));
-                // encryptedImage.style.display = 'block';
-                // downloadEncryptedLink.href = URL.createObjectURL(encryptedBlob);
-                // downloadEncryptedLink.style.display = 'block';
-    
-                // if (decryptedImage.style.display === 'block') {
-                //     decryptedImage.style.display = 'none';
-                //     downloadDecryptedLink.style.display = 'none';
-                // }
             };
     
             reader.readAsArrayBuffer(file);
@@ -167,23 +106,15 @@ document.addEventListener('DOMContentLoaded', function () {
             const file = imageInput2.files[0];                                                                 
             const reader = new FileReader();
     
-            reader.onload = function (event) {
-                // const decryptedImage = document.getElementById('decryptedImage');                              
+            reader.onload = function (event) {                             
                 const imageData = new Uint8Array(event.target.result);
                 const decryptionKey = parseInt(decryptionKeyInput.value, 10);
                 const decryptedData = decryptImage(imageData, decryptionKey);
                 decryptedImage.src = URL.createObjectURL(new Blob([decryptedData], { type: file.type }));
-                // decryptedImage.src = URL.createObjectURL(new Blob([new Uint8Array(decryptedData.buffer)], { type: file.type }));
                 decryptedImage.style.display = 'block';
                 downloadDecryptedLink.href = URL.createObjectURL(new Blob([decryptedData], { type: file.type }));
                 downloadDecryptedLink.href = URL.createObjectURL(new Blob([new Uint8Array(decryptedData.buffer)], { type: file.type }));
                 downloadDecryptedLink.style.display = 'block';
-    
-                // if (encryptedImage.style.display === 'block') {
-                //     encryptedImage.style.display = 'none';
-                //     downloadEncryptedLink.style.display = 'none';
-                // }
-
                 decryptShow.style.display = "block";
             };
     
